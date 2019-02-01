deab0380f19263f107658aa3b1615abf1342ca07
Terence Coudert <terence.coudert@sintef.no>
2019-02-01 13:18:16 +0100

    Add Formatter sub-classes in bindgen.py

--- tools/bindgen/bindgen.py
+++ tools/bindgen/bindgen.py
@@ -8,40 +8,53 @@ from pprint import pprint
 import CppHeaderParser
 
 
-FORTRAN_BIND_C = """
+FORTRAN_FCN_BIND_C = """
+!{cdecl}
   {returns} &
-  function {name}_c({arg_list}) &
-     bind(C,name="{name}")
-     import c_ptr, c_char, c_int
-{args}
+  function {name}_c({func_arg}) &
+    bind(C,name="{name}")
+    import {cimport}
+{decl_arg}
   end function {name}_c
 """
 
-FORTRAN_CALL_C = """
+FORTRAN_FCN_CALL_C = """
 {doc}
+!{cdecl}
   {returns} &
-  function {name}({arg_list})
-    {args}
-    {returns} :: ans
-
-    ans = {name}_c({argc_list})
-    {name} = ans
-
+  function {name}({func_arg})
+{decl_arg}
+    {returns} :: returns
+{f2c}
+    returns = {name}_c({func_argc})
+    {name} = returns
   end function {name}
 """
 
-TYPES = {'fortran': {'int': 'integer',
-                     'size_t': 'integer',
-                     'char': 'character'},
+FORTRAN_SUB_BIND_C = """
+!{cdecl}
+  subroutine {name}_c({func_arg}) &
+    bind(C,name="{name}")
+    import {cimport}
+{decl_arg}
+  end subroutine {name}_c
+"""
 
-         'fortran_bind_c': {'int': 'c_int',
-                            'size_t': 'c_int',
-                            'char': 'c_char'},
-                            
-         'fortran_call_c': {}
-         }
+FORTRAN_SUB_CALL_C = """
+{doc}
+!{cdecl}
+  subroutine {name}({func_arg})
+{decl_arg}
+{f2c}
+    call {name}_c({func_argc})
+  end subroutine {name}
+"""
 
 
+def format_template(tpl):
+    return tpl[1:len(tpl)]
+         
+         
 class Argument(object):
     
     """ Representing a function argument """
@@ -58,60 +71,11 @@ class Argument(object):
     def parse(self, obj):
         if isinstance(obj, dict):
             self.name = obj.get('name', '')
-            self.ctype = obj.get('raw_type', '')
+            self.ctype = obj.get('raw_type', '').strip('::')
             self.is_ptr = obj.get('pointer', 0)
             self.is_const = obj.get('const', 0)
             self.is_array = obj.get('array', 0)
             self.ctypes_type = obj.get('ctypes_type', 0)
-            
-    def get_type(self, lang):
-        if lang == 'fortran_return':
-            if (self.is_ptr > 0):
-                return 'type(c_ptr)'
-            else:
-                t1 = self.get_type('fortran')
-                t2 = self.get_type('fortran_bind_c')
-                return '{}({})'.format(t1, t2)
-            
-        return TYPES[lang].get(self.ctype, self.ctype)
-    
-    def declare(self, lang):
-        """ Return tuple (declare lines, copy lines) """
-        lines = []
-        copy = []
-        if lang == 'fortran_bind_c':
-            if self.is_ptr > 0:
-                if self.ctype == 'char':
-                    res = 'character(len=1,kind=c_char), dimension(*), '
-                    res += 'intent(in) :: ' + self.name
-                    lines.append(res)
-                elif self.ctype not in TYPES[lang]:
-                    lines.append('type(c_ptr), value :: ' + self.name)
-            if len(lines) == 0:
-                ans = self.declare('c')
-                lines.append('! ' + ans[0][0])
-        
-        elif lang == 'fortran_call_c': 
-            if self.is_ptr > 0:
-                if self.ctype == 'char':
-                    l1 = 'character(len=*), intent(in) :: {0}'
-                    l2 = 'character(len=1,kind=c_char) :: {0}_c(len_trim({0})+1)'
-                    lines.append(l1.format(self.name))
-                    lines.append(l2.format(self.name))
-                    c1 = '{0}_c = f_c_string_func({0})'
-                    copy.append(c1.format(self.name))
-                elif self.ctype not in TYPES[lang]:
-                    lines.append('type(c_ptr) :: ' + self.name)
-            if len(lines) == 0: 
-                ans = self.declare('c')
-                lines.append('! ' + ans[0][0])
-
-        elif lang == 'c':
-            lines.append('{} {}{};'.format(self.ctype,
-                                           '*' if self.is_ptr else '',
-                                           self.name))
-
-        return (lines, copy)
 
 
 class Function(object):
@@ -140,52 +104,253 @@ class Function(object):
                 if arg.name:
                     self.args.append(arg)
             
-    def fortran(self, key):
-        if key == 'doc':
-            sep = '!{}\n'.format('-' * 75)
-            doc = sep
-            for line in self.doxygen.split('\n'):
-                doc += '!>{}\n'.format(line)
-            doc += sep
-            return doc
-        
-        elif key == 'public':
-            return '  public :: ' + self.name
-        
-        elif key == 'bind_c':
-            lang = 'fortran_bind_c'
-            args = [arg.name for arg in self.args]
-            ctypes = [arg.get_type(lang) for arg in self.args]
-            dc = []
-            cp = []
-            for item in [arg.declare(lang) for arg in self.args]:
-                dc += ['     {}'.format(x) for x in item[0]]
-                cp += ['     {}'.format(x) for x in item[1]]
-            return FORTRAN_BIND_C.format(
-                    returns=self.returns.get_type('fortran_return'),
-                    name=self.name,
-                    arg_list=', '.join(args),
-                    ctype_list=', '.join(ctypes),
-                    args='{}\n{}'.format('\n'.join(dc), '\n'.join(cp))
-                   )
+
+class Formatter(object):
+    
+    def func(self, fcn, fmt):
+        """ Format a function with the specific format """
+        return ''
+    
+    def arg(self, arg, fmt):
+        """ Format an argument with the specific format """
+        return ''
+
+    def module(self, mod):
+        """ Format a module/header file """
+        return ''
+    
+    
+class CFormat(Formatter):
+    
+    """ Formatter for the C language """
+    
+    decl_arg = '{ctype} {pointer}{name};'
+    func_arg = '{ctype} {pointer}{name}'
+    decl_func = '{returns} {pointer}{name}({func_arg});'
+    impl_func = '{returns} {pointer}{name}({func_arg}) {\n};'
+    
+    def prop(self, obj):
+        """ Return the c properties of an argument """
+        if isinstance(obj, Argument):
+            return {'pointer': '*' * obj.is_ptr,
+                    'name': obj.name,
+                    'ctype': obj.ctype}
             
-        elif key == 'call_c':
-            lang = 'fortran_call_c'
-            arg_list = [arg.name for arg in self.args]
-            dc = []
-            cp = []
-            for item in [arg.declare(lang) for arg in self.args]:
-                dc += ['     {}'.format(x) for x in item[0]]
-                cp += ['     {}'.format(x) for x in item[1]]            
-            return FORTRAN_CALL_C.format(
-                    doc=self.fortran('doc'),
-                    returns=self.returns.get_type('fortran_return'),
-                    name=self.name,
-                    arg_list=', '.join(arg_list),
-                    argc_list=', '.join([arg + '_c' for arg in arg_list]),
-                    args='{}\n{}'.format('\n'.join(dc), '\n'.join(cp))
-                   )            
+        elif isinstance(obj, Function):
+            return {'pointer': '*' * obj.returns.is_ptr,
+                    'returns': obj.returns.ctype,
+                    'name': obj.name,
+                    'func_arg': self.func(obj, 'func_arg')}
+                        
+    def func(self, fcn, fmt):
+        """ Format a function with the specific format """
+        if fmt == 'func_arg':
+            args = [self.func_arg.format(**self.prop(arg)) for arg in fcn.args]
+            return ', '.join(args)
+        elif fmt == 'decl_arg':
+            args = [self.decl_arg.format(**self.prop(arg)) for arg in fcn.args]
+            return '\n'.join(args)
+        elif fmt == 'decl_func':
+            return self.decl_func.format(**self.prop(fcn))
             
+    def arg(self, arg, fmt):
+        """ Format an argument with the specific format """
+        prop = self.prop(arg)
+        if hasattr(self, fmt):
+            return getattr(self, fmt).format(**prop)
+        else:
+            return ''
+    
+    
+class FortranFormat(Formatter):
+    
+    """ Formatter for the Fortran language """
+    
+    decl_arg = '{ftype}{comma}{attr} :: {name}'
+    func_arg = '{name}'
+    func_argc = '{name}_c'
+    public = '  public :: {name}'
+    fcn_bind_c = format_template(FORTRAN_FCN_BIND_C)
+    fcn_call_c = format_template(FORTRAN_FCN_CALL_C)
+    sub_bind_c = format_template(FORTRAN_SUB_BIND_C)
+    sub_call_c = format_template(FORTRAN_SUB_CALL_C)
+    f2c_str = '{name}_c = f_c_string_func({name})'
+    
+    def __init__(self):
+        self.cfmt = CFormat()
+    
+    def c_type(self, arg):
+        """ Return the iso_c_binding type for Fortran """        
+        if arg.ctype == 'char':
+            return 'c_char'        
+        
+        if arg.is_ptr > 0:
+            return 'c_ptr'
+        
+        if arg.ctype == 'size_t':
+            return 'c_size_t'
+        
+        elif arg.ctype.find('int') >= 0:
+            items = arg.ctype.split(' ')
+            if len(items) > 1:
+                return 'c_{}'.format('_'.join(items[0:-1]))
+            else:
+                return 'c_' + arg.ctype
+            
+        elif arg.ctype.find('float128') >= 0:
+            return 'c_float128'
+        
+        elif arg.ctype in ['float', 'double', 'long double']:
+            return 'c_' + arg.ctype.replace(' ', '_')
+        
+        else:
+            return 'c_ptr'
+            
+    def ftype(self, arg, length='', bind_c=False):
+        """ Return the Fortran type of the given C type """   
+        ctype = arg.ctype
+        ftype = 'type'
+        if ctype.find('int') >= 0:
+            ftype = 'integer'
+        elif ctype == 'size_t':
+            ftype = 'integer'
+        elif ctype.find('double') >= 0:
+            ftype = 'real'
+        elif ctype.find('float') >= 0:
+            ftype = 'real'
+        elif ctype.find('bool') >= 0:
+            ftype = 'logical'
+        elif ctype == 'char':
+            ftype = 'character'
+
+        attr = []
+        if ctype == 'char':
+            if length:
+                attr.append('len={}'.format(length))
+            if bind_c:
+                attr.append('kind=c_char')
+        elif bind_c or (ftype == 'type'):
+            attr.append(self.c_type(arg))
+        if attr:
+            return '{ftype}({attr})'.format(ftype=ftype, attr=','.join(attr))
+        else:
+            return ftype
+    
+    def func(self, fcn, fmt):
+        """ Format a function with the specific format """
+        if fmt.find('func_arg') == 0:
+            return ', '.join([self.arg(arg, fmt) for arg in fcn.args])
+
+        elif fmt == 'doc':
+            sep = '!{}'.format('-' * 75)
+            doc = [sep]
+            for line in fcn.doxygen.split('\n'):
+                lin = line.replace('/**', '').replace('*/', '')
+                doc.append('!>{}'.format(lin))
+            doc.append(sep)
+            return '\n'.join(doc)
+        
+        elif fmt == 'public':
+            return self.public.format(name=fcn.name)
+        
+        elif fmt == 'bind_c':
+            sub = fcn.returns.ctype == 'void'
+            prop = {}
+            prop['cdecl'] = self.cfmt.func(fcn, 'decl_func')
+            prop['name'] = fcn.name
+            if not sub:
+                prop['returns'] = self.ftype(fcn.returns, bind_c=True)
+            prop['func_arg'] = self.func(fcn, 'func_arg')
+            ctyp = [self.c_type(arg) for arg in fcn.args]
+            if not sub:
+                ctyp.append(self.c_type(fcn.returns))
+            prop['cimport'] = ', '.join(set(ctyp))
+            decl_arg = []
+            for arg in fcn.args:
+                item = self.arg(arg, 'decl_arg',
+                                length=1,
+                                dimension='*',
+                                intent='in',
+                                bind_c=True)
+                decl_arg.append('    ' + item)
+            prop['decl_arg'] = '\n'.join(decl_arg)
+            if sub:
+                return self.sub_bind_c.format(**prop)
+            else:
+                return self.fcn_bind_c.format(**prop)
+            
+        elif fmt == 'call_c':
+            prop = {}
+            prop['name'] = fcn.name
+            prop['doc'] = self.func(fcn, 'doc')
+            prop['cdecl'] = self.cfmt.func(fcn, 'decl_func')
+            prop['func_arg'] = self.func(fcn, 'func_arg')
+            prop['func_argc'] = self.func(fcn, 'func_argc')
+            decl_arg = []
+            ind = '    '
+            for arg in fcn.args:
+                decl_arg.append(ind + self.arg(arg, 'decl_arg'))
+            for arg in fcn.args:
+                item = self.arg(arg, 'decl_argc', bind_c=True, length=1)
+                if arg.ctype == 'char':
+                    item += '(len_trim({})+1)'.format(arg.name)
+                decl_arg.append(ind + item)
+            prop['decl_arg'] = '\n'.join(decl_arg)
+            sub = fcn.returns.ctype == 'void'
+            if not sub:
+                prop['returns'] = self.ftype(fcn.returns)
+            f2c = []
+            for arg in fcn.args:
+                if (arg.ctype == 'char') & (arg.is_ptr == 1):
+                    f2c.append(ind + self.f2c_str.format(name=arg.name))
+            prop['f2c'] = '\n'.join(f2c)
+            if sub:
+                return self.sub_call_c.format(**prop)
+            else:
+                return self.fcn_call_c.format(**prop)
+                
+    def arg(self, arg, fmt='', **kwargs):
+        """ Format an argument with the specific format """
+        if fmt == 'func_arg':
+            return self.func_arg.format(name=arg.name)
+        
+        elif fmt == 'func_argc':
+            return self.func_argc.format(name=arg.name)        
+        
+        elif fmt.find('decl_arg') == 0:
+            attr = []
+            bind_c = kwargs.get('bind_c', False)
+            if arg.ctype == 'char':
+                for key in ['intent', 'dimension']:
+                    val = kwargs.get(key, '')
+                    if val:
+                        attr.append('{}({})'.format(key, val))
+            elif ((arg.is_ptr > 0) | (arg.ctype != 'char')) and bind_c:
+                attr.append('value')
+                
+            if kwargs.get('parameter', False):
+                attr.append('parameter')
+                
+            ftype = self.ftype(arg, kwargs.get('length', ''), bind_c)
+            name = arg.name + '_c' if fmt == 'decl_argc' else arg.name
+            return self.decl_arg.format(ftype=ftype,
+                                        comma=',' if attr else '',
+                                        attr=','.join(attr),
+                                        name=name)
+            
+    def module(self, mod, template, filename):
+        """ Generate Fortran module """
+        with open(template) as fil:
+            txt = fil.read()
+            
+        arg = {}
+        for key in ['public', 'bind_c', 'call_c']:
+            items = [self.func(fcn, key) for fcn in mod.functions]
+            arg[key] = '\n'.join(items)
+            
+        with open(filename, 'w') as fil:
+            fil.write(txt.format(**arg))
+    
             
 class Headers(object):
     
@@ -198,19 +363,7 @@ class Headers(object):
         for fcn in header.functions:
             ff = Function(fcn)
             if ff.name:
-                self.functions.append(ff)        
-        
-    def generate_fortran(self):
-        """ Generate dlite Fortran module """
-        with open('template-dlite.f90') as fil:
-            txt = fil.read()
-            
-        arg = {}
-        for key in ['public', 'bind_c', 'call_c']:
-            arg[key] = '\n'.join([fcn.fortran(key) for fcn in self.functions])
-            
-        with open('dlite.f90', 'w') as fil:
-            fil.write(txt.format(**arg))
+                self.functions.append(ff)
         
         
 if __name__ == '__main__':
@@ -218,5 +371,7 @@ if __name__ == '__main__':
     dlite = Headers()
     dlite.parse('../../src/dlite-entity.h')
     dlite.parse('../../src/dlite-storage.h')
-    dlite.generate_fortran()
+    
+    fmt = FortranFormat()
+    fmt.module(dlite, 'template-dlite.f90', 'dlite.f90')
     
