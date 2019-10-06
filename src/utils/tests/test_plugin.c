f9cb382c1893065fe258a6e17241d22ae6d0d6eb
Jesper Friis <jesper.friis@sintef.no>
2019-10-06 13:43:33 +0200

    Added test for plugin iterator

--- src/utils/tests/test_plugin.c
+++ src/utils/tests/test_plugin.c
@@ -26,8 +26,26 @@ MU_TEST(test_info_create)
 MU_TEST(test_get_api)
 {
   const TestAPI *api;
-
   mu_check((api = plugin_get_api(info, "testapi")));
+  mu_assert_string_eq("testapi", api->name);
+  mu_assert_int_eq(4, api->fun1(1, 3));
+  mu_assert_double_eq(6.28, api->fun2(3.14));
+}
+
+
+MU_TEST(test_iter)
+{
+  int n=0;
+  const void *p;
+  PluginIter iter;
+  const TestAPI *api;
+  plugin_api_iter_init(&iter, info);
+  while ((p = plugin_api_iter_next(&iter))) {
+    api = (const TestAPI *)p;
+    n++;
+  }
+  mu_assert_int_eq(1, n);
+  mu_assert_string_eq("testapi", api->name);
 }
 
 
@@ -54,6 +72,7 @@ MU_TEST_SUITE(test_suite)
 {
   MU_RUN_TEST(test_info_create);       /* setup */
   MU_RUN_TEST(test_get_api);
+  MU_RUN_TEST(test_iter);
   MU_RUN_TEST(test_unload);
   MU_RUN_TEST(test_info_free);         /* tear down */
 }
