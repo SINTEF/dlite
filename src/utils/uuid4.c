d109495806720fdc483445a52c35f415df3d4383
Jesper Friis <jesper.friis@sintef.no>
2020-09-06 12:26:53 +0200

    Improved seeding random UUIDs when CryptAcquireContext fails on Windows...

--- src/utils/uuid4.c
+++ src/utils/uuid4.c
@@ -37,14 +37,32 @@ static uint64_t xorshift128plus(uint64_t *s) {
 }
 
 
+#if RAND_MAX/256 >= 0xFFFFFFFFFFFFFF
+  #define LOOP_COUNT 1
+#elif RAND_MAX/256 >= 0xFFFFFF
+  #define LOOP_COUNT 2
+#elif RAND_MAX/256 >= 0x3FFFF
+  #define LOOP_COUNT 3
+#elif RAND_MAX/256 >= 0x1FF
+  #define LOOP_COUNT 4
+#else
+  #define LOOP_COUNT 5
+#endif
+uint64_t rand_uint64(void) {
+  uint64_t r = 0;
+  for (int i=LOOP_COUNT; i > 0; i--) {
+    r = r*(RAND_MAX + (uint64_t)1) + rand();
+  }
+  return r;
+}
+
+
 static int simple_seed(void) {
   int i;
   clock_t c = clock();
   srand((unsigned int)c);
-  for (i=0; i<2; i++) {
-    uint64_t x = 0x0000ffff & rand();
-    seed[i] = (x << 32) | (0x0000ffff & rand());
-  }
+  for (i=0; i<2; i++)
+    seed[i] = rand_uint64();
   return UUID4_ESUCCESS;
 }
 
