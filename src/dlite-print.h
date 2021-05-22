cca98d3dd405c4f7cf1281b4782ff21b0d632772
Jesper Friis <jesper.friis@sintef.no>
2021-05-22 11:31:54 +0200

    Added dlite-print.c

---
+++ src/dlite-print.h
@@ -0,0 +1,16 @@
+#ifndef _DLITE_PRINT_H
+#define _DLITE_PRINT_H
+
+
+typedef enum {
+  dlitePrintUUID,  /*!< Whether to include uuid */
+} DLitePrintFlag;
+
+/**
+  Prints instance `inst` to buffer, formatted as JSON.
+*/
+int dlite_print(char *dest, size_t size, DLiteInstance *inst,
+                         int indent, DLitePrintFlag flags);
+
+
+#endif /*_ DLITE_PRINT_H */
