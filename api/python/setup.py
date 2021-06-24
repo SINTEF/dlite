21d230c2d690d8d8f96420f3f8f0a40a60431161
Peter Schiffels <peter.schiffels@ifam.fraunhofer.de>
2021-06-24 11:27:44 +0200

    wip: Experimenting with a different setup.py from https://github.com/galois-advertising/cmake_setup/blob/master/cmake_setup/cmake/__init__.py

--- api/python/setup.py
+++ api/python/setup.py
@@ -1,94 +1,111 @@
 import os
 import sys
 import re
-import pathlib
+import subprocess
+
+from pathlib import Path
 from glob import glob
 from sysconfig import get_path, get_config_var
 
-from distutils import sysconfig
+import platform
 
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext as build_ext_orig
+from distutils import sysconfig, dir_util
 
+from setuptools import setup, Extension, Distribution
+from setuptools.command.build_ext import build_ext
 
-source_dir = pathlib.Path(__file__).parent.absolute()
+# Based on
+# https://github.com/galois-advertising/cmake_setup/blob/master/cmake_setup/cmake/__init__.py
 
-# Path to dlite package installed by CMake
-dlite_package = os.path.join(
-    'dist',
-    os.path.relpath(get_path('platlib'), get_config_var('exec_prefix')),
-    'dlite')
+SETUP_DIR = Path(__file__).parent.resolve()
+SOURCE_DIR = (SETUP_DIR / "../.." ).resolve()
+BUILD_DIR = SETUP_DIR / "mybuild"
 
+CONFIG="Release"
 
-def rglob(patt):
-    """Recursive glob function that only returns ordinary files."""
-    return [f for f in glob(patt, recursive=True) if os.path.isfile(f)]
+BUILD_DIR.mkdir(exist_ok=True)
+
+CMAKE_COMMON_VARIABLES = [
+    #'-G', 'Visual Studio 15 2017',
+    #'-A', 'x64',
+    '-DWITH_DOC=OFF',
+    '-DWITH_JSON=ON',
+    '-DBUILD_JSON=ON',
+    '-DWITH_HDF5=OFF',
+    '-DWITH_PYTHON_BUILD_WHEEL=ON',
+    #'-DCMAKE_BUILD_TYPE=%s' % config,
+    #'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=%s' % extdir.parent.absolute(),
+    #'-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=%s' % binary_dir,
+    #f'-DCMAKE_INSTALL_PREFIX={install_dir}',
+    #'-DALLOW_WARNINGS=ON',
+    # Assume cmake version >= v3.12.4
+    #'-DPython_EXECUTABLE=%s' % sys.executable,
+    #'-DPYTHON_EXECUTABLE=%s' % sys.executable,
+    #str(SOURCE_DIR)
+]
 
 
 class CMakeExtension(Extension):
+    """
+    setuptools.Extension for cmake
+    """
 
-    def __init__(self, name):
-        # don't invoke the original build_ext for this special extension
-        super().__init__(name, sources=[])
-
-
-class build_ext(build_ext_orig):
-
-    def run(self):
-        for ext in self.extensions:
-            self.build_cmake(ext)
-        super().run()
-
-    def build_cmake(self, ext):
-        cwd = pathlib.Path().absolute()
-
-        # these dirs will be created in build_py, so if you don't have
-        # any python sources to bundle, the dirs will be missing
-        binary_dir = pathlib.Path(self.build_temp)
-        binary_dir.mkdir(parents=True, exist_ok=True)
-        #extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
-        #extdir.mkdir(parents=True, exist_ok=True)
-
-        # example of cmake args
-        config = 'Debug' if self.debug else 'Release'
-        cmake_args = [
-            '-DCMAKE_BUILD_TYPE=%s' % config,
-            #'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=%s' % extdir.parent.absolute(),
-            #'-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=%s' % binary_dir,
-            '-DCMAKE_INSTALL_PREFIX=%s/dist' % source_dir,
-            '-DALLOW_WARNINGS=ON',
-            # Assume cmake version >= v3.12.4
-            #'-DPython_EXECUTABLE=%s' % sys.executable,
-            '-DPYTHON_EXECUTABLE=%s' % sys.executable,
-        ]
-
-        # example of build args
-        build_args = [
-            '--config', config,
-            '--', '-j4'
-        ]
-
-        os.chdir(str(binary_dir))
-        self.spawn(['cmake', f'{source_dir}'] + cmake_args)
-        if not self.dry_run:
-            self.spawn(['cmake', '--build', '.'] + build_args)
-            self.spawn(['cmake', '--install', '.'])
-
-        # Troubleshooting: if fail on line above then delete all possible
-        # temporary CMake files including "CMakeCache.txt" in top level dir.
-        os.chdir(str(cwd))
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
 
-# Extract current version from CMakeLists
-cmakelists = pathlib.Path(source_dir / 'CMakeLists.txt')
-version, = re.search(r'project\([^)]*VERSION\s+([0-9.]+)',
-                    cmakelists.read_text()).groups()
+class CMakeBuildExt(build_ext):
+    """
+    setuptools build_exit which builds using cmake & make
+    You can add cmake args with the CMAKE_COMMON_VARIABLES environment variable
+    """
+
+    def build_extension(self, ext):
+        if isinstance(ext, CMakeExtension):
+            output_dir = os.path.abspath(
+                os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+            build_type = 'Debug' if self.debug else 'Release'
+            cmake_args = ['cmake',
+                          str(SOURCE_DIR),
+                          '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + output_dir,
+                          '-DCMAKE_BUILD_TYPE=' + build_type,
+                          ]
+            cmake_args.extend(CMAKE_COMMON_VARIABLES)
+
+            env = os.environ.copy()
+            if not os.path.exists(self.build_temp):
+                os.makedirs(self.build_temp)
+            #self.build_temp=build\temp.win-amd64-3.8\Release
+            #ext.sourcedir=C:\Users\ps-adm\repo\proj\A419409_OntoTRANS\SINTEF\dlite-github\api\python
+            print(" ".join(cmake_args))
+            subprocess.check_call(cmake_args,
+                                  cwd=self.build_temp,
+                                  env=env)
+            #subprocess.check_call(['make', '-j', ext.name],
+            #                      cwd=self.build_temp,
+            #                      env=env)
+        else:
+            super().build_extension(ext)
+
+
+requirements = [
+    "numpy==1.19.2",
+    "PyYAML",
+    "psycopg2-binary",
+    "pandas",
+    "tables"
+    ]
+
+version = re.search(r'project\([^)]*VERSION\s+([0-9.]+)', Path(SOURCE_DIR / 'CMakeLists.txt').read_text()).groups()[0]
 
 setup(
-    name='dlite',
+    name='dlite-python',
     version=version,
     author='SINTEF',
     author_email='jesper.friis@sintef.no',
+    platforms=[ "Windows", "Linux" ],
     description=('Lightweight data-centric framework for working with '
                  'scientific data'),
     url='https://github.com/SINTEF/dlite',
@@ -108,23 +125,32 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     #download_url=['https://github.com/SINTEF/dlite/archive/v0.2.5.tar.gz'],
-    install_requires=['numpy', 'PyYAML', 'psycopg2'],
-    packages=[''],
-    package_data={'': rglob(dlite_package)},
-    ext_modules=[CMakeExtension(os.path.join(dlite_package, '_dlite'))],
+    install_requires=requirements,
+
+    #package_dir = { "dlite": r"../../out/build/x64-Release/bindings/python/dlite" }, # Must be relative
+    #!package_dir = { "dlite": "packages/dlite" },
+
+    #!packages= [ "dlite" ],
+    #!package_data={ "dlite": [ dlite_compiled_ext, '*.dll', './share/dlite/storage-plugins/*dll'] },
+    #!include_package_data=True,
+    #!has_ext_modules=lambda: True,
+    ext_modules=[CMakeExtension('make_target')],
+    cmdclass={
+        'build_ext': CMakeBuildExt,
+    },
+    #!distclass=BinaryDistribution,
+
     # FIXME: according to the setuptools documentation data_files is
     # deprecated and should be avoided since it doesn't work with weels.
     #
     # No alternative is mentioned in the documentation, though.  The
     # best I can think of is to use package_data and a post-install script
     # to move them to the right place.
-    data_files=[
-        ('include/dlite', rglob('dist/include/dlite/**')),
-        ('lib', rglob('dist/lib/**')),
-        ('share/dlite', rglob('dist/share/**')),
-        ('bin', glob('dist/bin/*')),
-    ],
-    cmdclass={
-        'build_ext': build_ext,
-    }
+    #data_files=[
+    #    ('include/dlite', rglob('dist/include/dlite/**')),
+    #    ('lib', rglob('dist/lib/**')),
+    #    ('share/dlite', rglob('dist/share/**')),
+    #    ('bin', glob('dist/bin/*')),
+    #],
+    zip_safe=False,
 )
