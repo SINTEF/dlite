72c7668e782de8c3ce075e425bc420632e9a7b97
francescalb <francesca.l.bleken@sintef.no>
2021-10-14 15:05:07 +0200

    Added start of linking to ontology

---
+++ examples/dehydrogenation/reaction/link_ontology.py
@@ -0,0 +1,9 @@
+from ontopy import get_ontology
+
+#onto = get_ontology('https://raw.githubusercontent.com/BIG-MAP/BattINFO/master/battinfo.ttl').load()
+
+onto=get_ontology("../ontology/chemistry.ttl").load()
+
+
+
+
