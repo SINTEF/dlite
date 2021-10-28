23fef8e0ada8deb8a52c71ce5b632457806c896a
francescalb <francesca.l.bleken@sintef.no>
2021-10-28 15:46:42 +0200

    Done mapping between dlite entities and ontology
    chemistry.ttl is the ontology that has chemsitry related entities
    mapsTo.ttl has the mapping ObjectProperty needed for mapping
    the metadata ontology for dlite is downloaded from emmo-repo
    
    molecules  nd reaction are mapped in their respecitve
    folders = 'calculations'
    
    Generated ontologies with the mapping are called mapping.ttl in each
    folder and are generated with the map_***.py file.

---
+++ examples/dehydrogenation/reaction/map_reaction.py
@@ -0,0 +1,30 @@
+from ontopy import get_ontology, World
+
+# Note install emmopython from github, not pypi. 
+world = World()
+
+mapsTo_onto = world.get_ontology('../ontology/mapsTo.ttl').load(EMMObased=False)
+
+chemistry_onto = world.get_ontology('../ontology/chemistry.ttl').load()
+
+dlite_onto = world.get_ontology('https://raw.githubusercontent.com/'
+                          'emmo-repo/datamodel-ontology/master'
+                          '/dlitemodel.ttl').load(EMMObased=False)
+
+mapping = world.get_ontology('mapping.ttl')
+mapping.imported_ontologies.extend([mapsTo_onto, chemistry_onto, dlite_onto])
+
+with mapping:
+
+    substance_model = dlite_onto.Metadata('http://onto-ns.com/meta/0.1/Substance')
+    substance_model.mapsTo = chemistry_onto.MoleculeModel
+
+
+    substance_energy = dlite_onto.Metadata('http://onto-ns.com/meta/0.1/Substance#energy')
+    substance_energy.mapsTo = chemistry_onto.GroundStateEnergy
+
+mapping.save('mapping.ttl')
+
+
+
+
