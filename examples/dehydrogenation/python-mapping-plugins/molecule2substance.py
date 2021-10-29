20f24dae1814242d1cc95402c9d95dc7416df408
Jesper Friis <jesper.friis@sintef.no>
2021-10-29 10:42:26 +0200

    Added transformation

---
+++ examples/dehydrogenation/python-mapping-plugins/molecule2substance.py
@@ -0,0 +1,15 @@
+import dlite
+
+
+class Molecule2Substance(DLiteMappingBase):
+    name = 'Molecule2Substance'
+    input_uris = ['http://onto-ns.com/meta/0.1/Molecule']
+    output_uri = 'http://onto-ns.com/meta/0.1/Substance'
+    cost = 25
+
+    def map(self, instances):
+        molecule = instances[0]
+        substance = dlite.Instance(self.output_uri, [])
+        substance.id = molecule.name
+        substance.energy = molecule.ground_state_energy
+        return substance
