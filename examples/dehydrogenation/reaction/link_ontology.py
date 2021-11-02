890693e795bde87dc3e05f9befb642debc592e52
Jesper Friis <jesper.friis@sintef.no>
2021-11-02 18:56:31 +0100

    Cleaning up

--- examples/dehydrogenation/reaction/link_ontology.py
+++
@@ -1,9 +0,0 @@
-from ontopy import get_ontology
-
-#onto = get_ontology('https://raw.githubusercontent.com/BIG-MAP/BattINFO/master/battinfo.ttl').load()
-
-onto=get_ontology("../ontology/chemistry.ttl").load()
-
-
-
-
