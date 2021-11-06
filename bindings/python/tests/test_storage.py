6b49d52494bea8a205d54b097cdf20e88851a1d8
Jesper Friis <jesper.friis@sintef.no>
2021-11-06 14:55:34 +0100

    Updated python storage tests

--- bindings/python/tests/test_storage.py
+++ bindings/python/tests/test_storage.py
@@ -16,3 +16,29 @@ del s
 
 with dlite.Storage(url) as s2:
     myentity2 = dlite.Instance(s2, 'http://onto-ns.com/meta/0.1/MyEntity')
+
+# Create an instance
+inst = myentity(dims=[2, 3], id='my-data')
+inst['a-bool-array'] = True, False
+
+
+# Test json
+print('--- testing json')
+myentity.save('json:myentity.json')
+inst.save('json:inst.json')
+del inst
+inst = dlite.Instance('json:inst.json#my-data')
+
+
+# Test yaml
+print('--- testing yaml')
+inst.save('yaml:inst.yaml?mode=w')
+del inst
+inst = dlite.Instance('yaml:inst.yaml#my-data')
+
+
+# Test rdf
+print('--- testing rdf')
+inst.save('rdf:db.xml?mode=w;store=file;filename=inst.ttl;format=turtle')
+del inst
+inst = dlite.Instance('rdf:db.xml#my-data')  # XXX read from inst.ttl not db.xml
