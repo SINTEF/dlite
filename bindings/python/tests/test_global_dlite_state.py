91732be9ee355993597358a53c7d6b1512c74e24
Peter Schiffels <peter.schiffels@gmail.com>
2021-11-16 16:04:36 +0100

    Add test assertion

--- bindings/python/tests/test_global_dlite_state.py
+++ bindings/python/tests/test_global_dlite_state.py
@@ -3,6 +3,14 @@ from global_dlite_state_sub import assert_exists
 
 uuids_in_istore=len(dlite.istore_get_uuids())
 
+# When this test_global_dlite_state.py is run standalone, we have uuids_in_istore == 3 (Basic, hardcoded dlite instances,
+# since we have not added anything yet)
+
+# But when this test is run as a testcase in test_python_bindings.py, this test is executed in the same Python process
+# as previous tests and those previous tests have already populated dlites internal storage (which nicely illustrates the point).
+
+assert uuids_in_istore in [3, 12]
+
 coll = dlite.Collection()
 assert dlite.has_instance(coll.uuid)
 
