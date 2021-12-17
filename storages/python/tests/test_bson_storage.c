c0994d20be222ba1a2014b9dfe6e094ccd879563
Torgeir Ustad <Torgeir.Ustad@sintef.no>
2021-12-17 09:40:46 +0100

    Update BSON test
    It's no longer necessary to skip comparing 'uri'.
    
    Also ensure that lines are no more than 79 characters long.

--- storages/python/tests/test_bson_storage.c
+++ storages/python/tests/test_bson_storage.c
@@ -9,7 +9,8 @@ MU_TEST(test_save)
   DLiteStorage* s = NULL;
 
   // Load JSON metadata
-  char* url = "json://" STRINGIFY(DLITE_ROOT) "/src/tests/test-entity.json?mode=r";
+  char* url
+	  = "json://" STRINGIFY(DLITE_ROOT) "/src/tests/test-entity.json?mode=r";
   DLiteInstance* meta = (DLiteInstance*)dlite_meta_load_url(url);
   mu_check(meta);
   mu_check(dlite_instance_is_meta(meta));
@@ -23,9 +24,11 @@ MU_TEST(test_save)
   // Load JSON data (corresponding to the metadata above)
   url = STRINGIFY(DLITE_ROOT) "/src/tests/test-data.json";
   mu_check(s = dlite_storage_open("json", url, "mode=r"));
-  DLiteInstance* data1 = dlite_instance_load(s, "204b05b2-4c89-43f4-93db-fd1cb70f54ef");
+  DLiteInstance* data1
+	  = dlite_instance_load(s, "204b05b2-4c89-43f4-93db-fd1cb70f54ef");
   mu_check(dlite_instance_is_data(data1));
-  DLiteInstance* data2 = dlite_instance_load(s, "e076a856-e36e-5335-967e-2f2fd153c17d");
+  DLiteInstance* data2
+	  = dlite_instance_load(s, "e076a856-e36e-5335-967e-2f2fd153c17d");
   mu_check(dlite_instance_is_data(data2));
   
   // Save JSON data to BSON file
@@ -44,17 +47,19 @@ MU_TEST(test_load)
   DLiteStorage* s = NULL;
 
   // Load JSON metadata
-  char* url = "json://" STRINGIFY(DLITE_ROOT) "/src/tests/test-entity.json?mode=r";
+  char* url
+	  = "json://" STRINGIFY(DLITE_ROOT) "/src/tests/test-entity.json?mode=r";
   DLiteInstance* json_meta = (DLiteInstance*)dlite_meta_load_url(url);
   char* json_str1 = dlite_json_aprint(json_meta, 0, 1);
-  while (dlite_instance_decref(json_meta)); // Remove all references to json_meta
+  while (dlite_instance_decref(json_meta)); // Remove all json_meta references
 
   // Load BSON metadata
   mu_check(s = dlite_storage_open("bson", "test_meta.bson", "mode=r"));
-  DLiteInstance* bson_meta = dlite_instance_load(s, "2b10c236-eb00-541a-901c-046c202e52fa");
+  DLiteInstance* bson_meta
+	  = dlite_instance_load(s, "2b10c236-eb00-541a-901c-046c202e52fa");
   mu_check(bson_meta);
   char* bson_str1 = dlite_json_aprint(bson_meta, 0, 1);
-  while (dlite_instance_decref(bson_meta)); // Remove all references to bson_meta
+  while (dlite_instance_decref(bson_meta)); // Remove all bson_meta references
 
   // Compare JSON and BSON metadata
   mu_assert_int_eq(0, strcmp(json_str1, bson_str1));
@@ -63,28 +68,29 @@ MU_TEST(test_load)
   // Load JSON data (corresponding to the metadata above)
   url = STRINGIFY(DLITE_ROOT) "/src/tests/test-data.json";
   s = dlite_storage_open("json", url, "mode=r");
-  DLiteInstance* json_data = dlite_instance_load(s, "204b05b2-4c89-43f4-93db-fd1cb70f54ef");
+  DLiteInstance* json_data
+	  = dlite_instance_load(s, "204b05b2-4c89-43f4-93db-fd1cb70f54ef");
   json_str1 = dlite_json_aprint(json_data, 0, 1);
-  while (dlite_instance_decref(json_data)); // Remove all references to json_data
+  while (dlite_instance_decref(json_data)); // Remove all json_data references
   json_data = dlite_instance_load(s, "e076a856-e36e-5335-967e-2f2fd153c17d");
   char* json_str2 = dlite_json_aprint(json_data, 0, 1);
-  while (dlite_instance_decref(json_data)); // Remove all references to json_data
+  while (dlite_instance_decref(json_data)); // Remove all json_data references
 
   // Load BSON data
   mu_check(s = dlite_storage_open("bson", "test_data.bson", "mode=r"));
-  DLiteInstance* bson_data = dlite_instance_load(s, "204b05b2-4c89-43f4-93db-fd1cb70f54ef");
+  DLiteInstance* bson_data
+	  = dlite_instance_load(s, "204b05b2-4c89-43f4-93db-fd1cb70f54ef");
   mu_check(bson_data);
   bson_str1 = dlite_json_aprint(bson_data, 0, 1);
-  while (dlite_instance_decref(bson_data)); // Remove all references to bson_data
+  while (dlite_instance_decref(bson_data)); // Remove all bson_data references
   bson_data = dlite_instance_load(s, "e076a856-e36e-5335-967e-2f2fd153c17d");
   mu_check(bson_data);
   char* bson_str2 = dlite_json_aprint(bson_data, 0, 1);
-  while (dlite_instance_decref(bson_data)); // Remove all references to bson_data
+  while (dlite_instance_decref(bson_data)); // Remove all bson_data references
 
   // Compare JSON and BSON data
   mu_assert_int_eq(0, strcmp(json_str1, bson_str1));
-  mu_assert_int_eq(0, strncmp(json_str2, bson_str2, 47));
-  mu_assert_int_eq(0, strcmp(strstr(json_str2, "\"meta\""), strstr(bson_str2, "\"meta\"")));
+  mu_assert_int_eq(0, strcmp(json_str2, bson_str2));
   mu_assert_int_eq(0, dlite_storage_close(s));
 }
 
@@ -93,7 +99,7 @@ MU_TEST(test_unload_plugins)
   dlite_storage_plugin_unload_all();
 }
 
-/***********************************************************************/
+/****************************************************************************/
 
 MU_TEST_SUITE(test_suite)
 {
