e63cd17b9df7a6121337f75e478e51c43d40b9ff
Torgeir Ustad <Torgeir.Ustad@sintef.no>
2021-12-21 14:43:22 +0100

    Add script for running all tests

---
+++ storages/python-tests/run_python_tests.py
@@ -0,0 +1,26 @@
+import glob
+import os
+import runpy
+import shutil
+
+
+screen_width = shutil.get_terminal_size().columns - 1
+
+
+def print_test_exception(err: Exception):
+    print(''.center(screen_width, '#'))
+    print('Exception raised:')
+    print(str(err))
+    print(''.center(screen_width, '#'))
+
+
+if __name__ == '__main__':
+    # The test scripts to run
+    tests = []
+    path = str(os.getcwd()).replace('\\', '/') + '/'
+    for t in tests:
+        print(''.center(screen_width, '-')) 
+        try:
+            runpy.run_path(path + t)
+        except Exception as err:
+            print_test_exception(err)
