b7540d6d14107781b5f1be08212ffbbea3e67b4c
Jesper Friis <jesper-friis@users.noreply.github.com>
2022-08-08 07:30:06 +0200

    Fix segmentation fault for empty array type DLiteRef. (#298)

--- src/pyembed/dlite-pyembed.c
+++ src/pyembed/dlite-pyembed.c
@@ -292,13 +292,18 @@ void *dlite_pyembed_get_address(const char *symbol)
 
 
 /*
-  Returns a Python representation of dlite instance with given id or NULL
-  on error.
+  Returns a Python representation of dlite instance with given id or
+  Py_None if `id` is NULL.
+
+  On error NULL is returned.
 */
 PyObject *dlite_pyembed_from_instance(const char *id)
 {
   PyObject *pyid=NULL, *dlite_name=NULL, *dlite_module=NULL, *dlite_dict=NULL;
   PyObject *get_instance=NULL, *instance=NULL;
+
+  if (!id)
+    Py_RETURN_NONE;
 
   if (!(pyid = PyUnicode_FromString(id)))
     FAIL("cannot create python string");
