f28cda19904d3a0162b0315c6814f54205b050c4
Jesper Friis <jesper-friis@users.noreply.github.com>
2022-08-23 19:56:25 +0200

    Support labels in dlite.triplestore (#300)
    * Added support for labels and checking to Namespace
    * Added test ontologies
    * Removed tests accessing the network
    * Implemented fixes suggested by Thomas Hagelien during review

--- bindings/python/triplestore/triplestore.py
+++ bindings/python/triplestore/triplestore.py
@@ -1,8 +1,10 @@
 '''A module encapsulating different triplestores using the strategy design
 pattern.
 
-This module exposes the public interface of the triplestore package.
-It has no dependencies outside the standard library.  However the
+See https://github.com/SINTEF/dlite/tree/master/bindings/python/triplestore
+for an introduction.
+
+This module has no dependencies outside the standard library, but the
 triplestore backends may have.
 '''
 from __future__ import annotations  # Support Python 3.7 (PEP 585)
@@ -36,23 +38,154 @@ class UniquenessError(TriplestoreError):
 class NamespaceError(TriplestoreError):
     """Namespace error."""
 
+class NoSuchIRIError(NamespaceError):
+    """Namespace has no such IRI."""
+
 
 class Namespace:
-    """Represent a namespace."""
-    def __init__(self, uri):
-        self.uri = str(uri)
+    """Represent a namespace.
+
+    Arguments:
+        iri: IRI of namespace to represent.
+        label_annotations: Sequence of label annotations. If given, check
+            the underlying ontology during attribute access if the name
+            correspond to a label. The label annotations should be ordered
+            from highest to lowest precedense.
+            If True is provided, `label_annotations` is set to
+            ``(SKOS.prefLabel, RDF.label, SKOS.altLabel)``.
+        check: Whether to check underlying ontology if the IRI exists during
+            attribute access.  If true, NoSuchIRIError will be raised if the
+            IRI does not exist in this namespace.
+        cachemode: Should be one of:
+              - Namespace.NO_CACHE: Turn off caching.
+              - Namespace.USE_CACHE: Cache attributes as they are looked up.
+              - Namespace.ONLY_CACHE: Cache all names at initialisation time.
+                Do not access the triplestore after that.
+            Default is `NO_CACHE` if neither `label_annotations` or `check`
+            is given, otherwise `USE_CACHE`.
+        triplestore: Use this triplestore for label lookup and checking.
+            If not given, and either `label_annotations` or `check` are
+            enabled, a new rdflib triplestore will be created.
+        triplestore_url: Alternative URL to use for loading the underlying
+            ontology if `triplestore` is not given.  Defaults to `iri`.
+    """
+    NO_CACHE = 0
+    USE_CACHE = 1
+    ONLY_CACHE = 2
+
+    __slots__ = (
+        "_iri", "_label_annotations", "_check", "_cache", "_triplestore",
+    )
+
+    def __init__(self, iri, label_annotations=(), check=False, cachemode=-1,
+                 triplestore=None, triplestore_url=None):
+        if label_annotations is True:
+            label_annotations = (SKOS.prefLabel, RDF.label, SKOS.altLabel)
+
+        self._iri = str(iri)
+        self._label_annotations = tuple(label_annotations)
+        self._check = bool(check)
+
+        need_triplestore = True if check or label_annotations else False
+        if cachemode == -1:
+            cachemode = (
+                Namespace.ONLY_CACHE if need_triplestore else Namespace.NO_CACHE
+            )
+
+        if need_triplestore and triplestore is None:
+            url = triplestore_url if triplestore_url else iri
+            triplestore = Triplestore("rdflib", base_iri=iri)
+            triplestore.parse(url)
+
+        self._cache = {} if cachemode != Namespace.NO_CACHE else None
+        #
+        # FIXME:
+        # Change this to only assigning the triplestore if cachemode is
+        # ONLY_CACHE when we figure out a good way to pre-populate the
+        # cache with IRIs from the triplestore.
+        #
+        #self._triplestore = (
+        #    triplestore if cachemode != Namespace.ONLY_CACHE else None
+        #)
+        self._triplestore = triplestore if need_triplestore else None
+
+        if cachemode != Namespace.NO_CACHE:
+            self._update_cache(triplestore)
+
+    def _update_cache(self, triplestore=None):
+        """Update the internal cache from `triplestore`."""
+        if not triplestore:
+            triplestore = self._triplestore
+        if not triplestore:
+            raise NamespaceError(
+                "`triplestore` argument needed for updating the cache"
+            )
+        if self._cache is None:
+            self._cache = {}
+
+        # Add (label, full_iri) pairs to cache
+        for la in reversed(self._label_annotations):
+            self._cache.update(
+                (o, s) for s, o in triplestore.subject_objects(la)
+                if s.startswith(self._iri)
+            )
+
+        # Add (name, full_iri) pairs to cache
+        # Currently we only check concepts that defines RDFS.isDefinedBy
+        # relations.
+        # Is there an efficient way to loop over all IRIs in this namespace?
+        n = len(self._iri)
+        self._cache.update(
+            (s[n:], s) for s in triplestore.subjects(
+                RDFS.isDefinedBy, self._iri)
+            if s.startswith(self._iri)
+        )
 
     def __getattr__(self, name):
-        return self.uri + name
+        if self._cache is not None and name in self._cache:
+            return self._cache[name]
+
+        if self._triplestore:
+
+            # Check if ``iri = self._iri + name`` is in the triplestore.
+            # If so, add it to the cache.
+            # We only need to check that generator returned by
+            # `self._triplestore.predicate_objects(iri)` is non-empty.
+            iri = self._iri + name
+            g = self._triplestore.predicate_objects(iri)
+            try:
+                g.__next__()
+            except StopIteration:
+                pass
+            else:
+                if self._cache is not None:
+                    self._cache[name] = iri
+                return iri
+
+            # Check for label annotations matching `name`.
+            for la in self._label_annotations:
+                for s, o in self._triplestore.subject_objects(la):
+                    if o == name and s.startswith(self._iri):
+                        if self._cache is not None:
+                            self._cache[name] = s
+                        return s
+
+        if self._check:
+            raise NoSuchIRIError(self._iri + name)
+        else:
+            return self._iri + name
 
     def __getitem__(self, key):
-        return self.uri + key
+        return self.__getattr__(key)
 
     def __repr__(self):
-        return f"Namespace({self.uri})"
+        return f"Namespace({self._iri})"
 
     def __str__(self):
-        return self.uri
+        return self._iri
+
+    def __add__(self, other):
+        return self._iri + str(other)
 
 
 # Pre-defined namespaces
@@ -69,9 +202,6 @@ DOAP = Namespace("http://usefulinc.com/ns/doap#")
 PROV = Namespace("http://www.w3.org/ns/prov#")
 DCAT = Namespace("http://www.w3.org/ns/dcat#")
 TIME = Namespace("http://www.w3.org/2006/time#")
-GEO = Namespace("http://www.w3.org/2003/01/geo/wgs84_pos#")
-SDO = Namespace("http://schema.org/")
-VANN = Namespace("http://purl.org/vocab/vann/")
 FNO = Namespace("https://w3id.org/function/ontology#")
 
 EMMO = Namespace("http://emmo.info/emmo#")
@@ -202,22 +332,22 @@ class Triplestore:
         # "dm": DM,
     }
 
-    def __init__(self, backend: str, base_iri: str = None, **kwargs):
+    def __init__(self, name: str, base_iri: str = None, **kwargs):
         """Initialise triplestore using the backend with the given name.
 
         Parameters:
-            backend: Module name for backend.
+            name: Module name for backend.
             base_iri: Base IRI used by the add_function() method when adding
                 new triples.
             kwargs: Keyword arguments passed to the backend's __init__()
                 method.
         """
-        module = import_module(backend if "." in backend
-                      else "dlite.triplestore.backends." + backend)
-        cls = getattr(module, backend.title() + "Strategy")
+        module = import_module(name if "." in name
+                      else "dlite.triplestore.backends." + name)
+        cls = getattr(module, name.title() + "Strategy")
         self.base_iri = base_iri
         self.namespaces = {}
-        self.backend_name = backend
+        self.backend_name = name
         self.backend = cls(**kwargs)
         # Keep functions in the triplestore for convienence even though
         # they usually do not belong to the triplestore per se.
@@ -257,7 +387,7 @@ class Triplestore:
 
         if hasattr(self.backend, "namespaces"):
             for prefix, ns in self.backend.namespaces().items():
-                if prefix not in self.namespaces:
+                if prefix and prefix not in self.namespaces:
                     self.namespaces[prefix] = Namespace(ns)
 
     def serialize(self, destination=None, format="turtle", **kwargs):
@@ -287,8 +417,11 @@ class Triplestore:
         self._check_method("update")
         return self.backend.update(update_object=update_object, **kwargs)
 
-    def bind(self, prefix: str, namespace: str):
+    def bind(self, prefix: str, namespace: str, **kwargs):
         """Bind prefix to namespace and return the new Namespace object.
+
+        The new Namespace is created with `namespace` as IRI.
+        Keyword arguments are passed to the Namespace() constructor.
 
         If `namespace` is None, the corresponding prefix is removed.
         """
@@ -297,7 +430,7 @@ class Triplestore:
             ns = None
         else:
             ns = namespace if isinstance(namespace, Namespace) else Namespace(
-                namespace)
+                namespace, **kwargs)
             self.namespaces[prefix] = ns
 
         if hasattr(self.backend, "bind"):
@@ -419,7 +552,7 @@ class Triplestore:
     def prefix_iri(self, iri: str, require_prefixed: bool = False):
         """Return prefixed IRI.
 
-        This is the referse of expand_iri().
+        This is the reverse of expand_iri().
 
         If `require_prefixed` is true, a NamespaceError exception is raised
         if no prefix can be found.
