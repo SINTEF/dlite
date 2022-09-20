4de5f2f7615c384797cc406887481e2d5f73e8ed
Anders Eklund <ajeklund@users.noreply.github.com>
2022-09-20 12:42:22 +0200

    Regexp and base units

--- bindings/python/triplestore/units.py
+++ bindings/python/triplestore/units.py
@@ -4,6 +4,7 @@ Further description here.
 
 """
 from pint import UnitRegistry, Quantity
+import re
 from triplestore import Triplestore
 
 def load_qudt():
@@ -14,7 +15,22 @@ def load_qudt():
     print("Finished.")
     return ts
 
+def parse_qudt_dimension_vector(dimension_vector) -> dict:
+    dimensions = re.findall(r'[AELIMHTD]-?[0-9]+', dimension_vector)
 
+    # Base units defined by:
+    # https://qudt.org/schema/qudt/QuantityKindDimensionVector
+    # https://qudt.org/vocab/sou/SI
+    base_units = {
+        "A": "mol",
+        "E": "A",
+        "L": "m",
+        "I": "cd",
+        "M": "kg",
+        "H": "K",
+        "T": "s",
+        "D": "1",
+    }
 
 # Test code.
 
@@ -24,5 +40,10 @@ QUDTU = ts.bind("unit", "http://qudt.org/vocab/unit/", check=True)
 QUDT = ts.bind("unit", "http://qudt.org/schema/qudt/", check=True)
 
 for s, p, o in ts.triples([None, QUDT.hasDimensionVector, None]):
-    print(s)
+    print(s + " " + o)
+
+    unit = s.split("/")[-1]
+    dimension_vector = o.split("/")[-1]
+
+
 
