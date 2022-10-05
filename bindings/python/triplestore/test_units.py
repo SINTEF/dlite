daf63f2a002d09d3d5d6095ab9947d29c7d94751
Anders Eklund <ajeklund@users.noreply.github.com>
2022-10-05 13:07:41 +0200

    Make test_units runnable.

---
+++ bindings/python/triplestore/test_units.py
@@ -0,0 +1,37 @@
+from units import pint_registry_lines_from_qudt, prepare_cache_file_path
+from pint import UnitRegistry, Quantity
+
+pint_registry_lines = pint_registry_lines_from_qudt()
+
+print(f'Number of registry lines = {len(pint_registry_lines)}')
+
+# Print pint registry definition to file.
+registry_file_path = prepare_cache_file_path("pint_unit_registry.txt")
+with open(registry_file_path, "w") as f:
+    for line in pint_registry_lines:
+        f.write(f"{line}\n")
+
+# Populate an empty pint registry.
+#ureg = UnitRegistry(None)
+#for line in pint_registry_lines:
+#    ureg.define(line)
+
+# for i in range(0, 1751):
+#     print("Line number: " + str(i))
+#     print(pint_registry_lines[i])
+#     with open("test_output.txt", "w") as f:
+#         for line in pint_registry_lines[0:i]:
+#             f.write(f"{line}\n")
+#     ureg = UnitRegistry("test_output.txt")
+
+ureg = UnitRegistry(registry_file_path)
+
+# Test the registry.
+test_quantity1 = 1234 * ureg.M
+print(test_quantity1)
+
+test_quantity2 = 2345.6 * ureg.W_PER_K
+print(f'{test_quantity2} = {test_quantity2.to_base_units()}')
+
+test_quantity3 = test_quantity1 * test_quantity2
+print("".join([str(test_quantity3), " = ", str(test_quantity3.to_base_units()), " = ", "{:~}".format(test_quantity3.to_base_units())]))
