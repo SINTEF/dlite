f26a317f30b3fbb484163a8ba39278b8bafcef57
Jesper Friis <jesper.friis@sintef.no>
2022-10-22 11:54:31 +0200

    Reverted some changes in test_units.py

--- bindings/python/triplestore/test_units.py
+++ bindings/python/triplestore/test_units.py
@@ -2,7 +2,7 @@ from units import pint_registry_lines_from_qudt, prepare_cache_file_path, get_pi
 from pint import UnitRegistry, Quantity
 
 
-ureg = get_pint_registry(force_recreate=False)
+ureg = get_pint_registry(force_recreate=True)
 
 # Test the registry.
 test_quantity1 = 1234 * ureg.m
@@ -12,7 +12,7 @@ test_quantity2 = 2345.6 * ureg.W_PER_K
 assert str(test_quantity2) == "2345.6 W_PER_K"
 
 test_quantity3 = test_quantity1 * test_quantity2
-#print("".join([str(test_quantity3), " = ",
-#               str(test_quantity3.to_base_units()),
-#               " = ",
-#               "{:~}".format(test_quantity3.to_base_units())]))
+print("".join([str(test_quantity3), " = ",
+               str(test_quantity3.to_base_units()),
+               " = ",
+               "{:~}".format(test_quantity3.to_base_units())]))
