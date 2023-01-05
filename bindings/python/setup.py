95223664790995c07026d298e361b9b7fd42c6f8
Jesper Friis <jesper.friis@sintef.no>
2023-01-05 23:12:43 +0100

    Added dlite-validate tool

---
+++ bindings/python/setup.py
@@ -0,0 +1,23 @@
+"""setup.py script for installing scripts."""
+import re
+import sys
+from setuptools import setup
+from glob import glob
+
+
+with open('../../CMakeLists.txt', 'rt') as f:
+    match = re.search(r'VERSION +([0-9.]+)', f.read())
+    version, = match.groups()
+
+
+setup(
+    name='dlite',
+    version=version,
+    scripts=['scripts/dlite-validate'],
+    entry_points={
+        'console_scripts': [
+            'pip=pip:main',
+            f'pip-{sys.version[:3]}=pip:main',
+        ],
+    },
+)
