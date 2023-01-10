f38140d8be1e9b98a22099acc0886eda0a8a7638
Jesper Friis <jesper.friis@sintef.no>
2023-01-10 19:20:24 +0100

    Cleanup

--- bindings/python/scripts/setup.py
+++ bindings/python/scripts/setup.py
@@ -1,11 +1,11 @@
-"""setup.py script for installing scripts."""
+"""setup.py file for installing scripts."""
 import re
 import sys
 from setuptools import setup
 from glob import glob
 
 
-with open('../../CMakeLists.txt', 'rt') as f:
+with open('../../../CMakeLists.txt', 'rt') as f:
     match = re.search(r'VERSION +([0-9.]+)', f.read())
     version, = match.groups()
 
@@ -13,7 +13,7 @@ with open('../../CMakeLists.txt', 'rt') as f:
 setup(
     name='dlite',
     version=version,
-    scripts=['scripts/dlite-validate'],
+    scripts=['dlite-validate'],
     entry_points={
         'console_scripts': [
             'pip=pip:main',
