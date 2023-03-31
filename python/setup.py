fa144558d90b65a39509110d02238da1dfaa1c70
Bjørn Tore Løvfall <blov@sintef.no>
2023-03-31 11:58:29 +0200

    Added support for Python 3.11

--- python/setup.py
+++ python/setup.py
@@ -201,6 +201,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=requirements + extra_requirements,
