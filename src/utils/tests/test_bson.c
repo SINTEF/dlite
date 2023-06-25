099786618fcdb5d68223f1c3dd25eaeeeaf71af7
Jesper Friis <jesper.friis@sintef.no>
2023-06-25 18:20:46 +0200

    Added comment

--- src/utils/tests/test_bson.c
+++ src/utils/tests/test_bson.c
@@ -397,6 +397,7 @@ double help_issue556(double x)
 MU_TEST(test_issue556) {
   double inf = 1.0/0.0;
   double nan = 0.0/0.0;
+  /* Some interesting doubles to check... */
   double values_to_test[] = {
     1, 0, -1, 3.14, 2.73, -2.3, 1e-6, -1e-6, 1e8, -1e8,
     +0.0, -0.0, inf, -inf, nan
