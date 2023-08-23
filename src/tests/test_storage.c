e581f2eb328c1990ff11ec55a468ac0c3b663bd3
Jesper Friis <jesper.friis@sintef.no>
2023-08-23 02:09:34 +0200

    Debugging storage query

--- src/tests/test_storage.c
+++ src/tests/test_storage.c
@@ -49,6 +49,56 @@ MU_TEST(test_get_driver)
   mu_assert_string_eq("json", driver);
 }
 
+MU_TEST(test_storage_iter)
+{
+  /* Iterate over UUIDs of all instances in the storage */
+  char uuid[DLITE_UUID_LENGTH+1];
+  void *iter = dlite_storage_iter_create(s, NULL);
+  int n = 0;
+  printf("\nUUIDs (iterated over):\n");
+  while (dlite_storage_iter_next(s, iter, uuid) == 0) {
+    printf("  %s\n", uuid);
+    n++;
+  }
+  printf("\n");
+  dlite_storage_iter_free(s, iter);
+  mu_assert_int_eq(2, n);
+}
+
+MU_TEST(test_storage_iter_pattern)
+{
+  /* Iterate over UUIDs of instances who's metadata matches pattern */
+  char uuid[DLITE_UUID_LENGTH+1];
+  void *iter = dlite_storage_iter_create(s, "http://onto-ns.com/meta/0.1/test-entity");
+  int n = 0;
+  printf("\nUUIDs (pattern iteration, ok):\n");
+  while (dlite_storage_iter_next(s, iter, uuid) == 0) {
+    DLiteInstance *inst = dlite_instance_get(uuid);
+    printf("  %s\n", inst->uuid);
+    //dlite_instance_decref(inst);
+    n++;
+  }
+  printf("\n");
+  dlite_storage_iter_free(s, iter);
+  mu_assert_int_eq(2, n);
+}
+
+MU_TEST(test_storage_iter_bad_pattern)
+{
+  /* Iterate over UUIDs of instances who's metadata matches invalid pattern */
+  char uuid[DLITE_UUID_LENGTH+1];
+  void *iter = dlite_storage_iter_create(s, "xxx");
+  int n = 0;
+  printf("\nUUIDs (pattern iteration, bad):\n");
+  while (dlite_storage_iter_next(s, iter, uuid) == 0) {
+    printf("  %s\n", uuid);
+    n++;
+  }
+  printf("\n");
+  dlite_storage_iter_free(s, iter);
+  mu_assert_int_eq(0, n);
+}
+
 MU_TEST(test_plugin_iter)
 {
   int n=0;
@@ -103,6 +153,9 @@ MU_TEST_SUITE(test_suite)
   MU_RUN_TEST(test_idflag);
   MU_RUN_TEST(test_uuids);
   MU_RUN_TEST(test_get_driver);
+  MU_RUN_TEST(test_storage_iter);
+  MU_RUN_TEST(test_storage_iter_pattern);
+  MU_RUN_TEST(test_storage_iter_bad_pattern);
   MU_RUN_TEST(test_plugin_iter);
   MU_RUN_TEST(test_load_all);
 
