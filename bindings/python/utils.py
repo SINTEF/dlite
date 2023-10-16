a39621af9ff8ac5376efa0e5281af13c5f5d5567
Jesper Friis <jesper.friis@sintef.no>
2023-10-16 18:34:27 +0200

    Added suggestion by reviewer

--- bindings/python/utils.py
+++ bindings/python/utils.py
@@ -545,7 +545,7 @@ def get_referred_instances(inst, include_meta=False):
 
     would return the set `{coll, inst1, inst2, inst3, inst4, inst5}`.
 
-    It same set would be returned even if one of the instances would
+    The same set would be returned even if one of the instances would
     have a 'ref' property referring back to 'coll'.
     """
     references = set()
