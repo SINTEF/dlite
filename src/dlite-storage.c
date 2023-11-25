81f7161f2f56bf649d13926800bf7fb2d8d8e363
Jesper Friis <jesper.friis@sintef.no>
2023-11-25 21:11:10 +0100

    Mark when entering atexit Python atexit handler

--- src/dlite-storage.c
+++ src/dlite-storage.c
@@ -216,7 +216,7 @@ int dlite_storage_iter_next(DLiteStorage *s, void *iter, char *buf)
 void dlite_storage_iter_free(DLiteStorage *s, void *iter)
 {
   // Do not call iterFree() during atexit(), since it may lead to segfault
-  if (dlite_globals_get_state(ATEXIT_MARKER_ID)) return;
+  if (dlite_globals_in_atexit()) return;
 
   if (!s->api->iterFree)
     errx(1, "driver '%s' does not support iterFree()", s->api->name);
