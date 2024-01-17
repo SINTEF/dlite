50afb53791cc694e7dcbae338acb6acee9488b28
Jesper Friis <jesper.friis@sintef.no>
2024-01-17 17:13:42 +0100

    Removed commented out code

--- bindings/python/tests/test_collection_load.py
+++ bindings/python/tests/test_collection_load.py
@@ -23,24 +23,3 @@ assert Coll["dimensions"].size
 d = json.load(open(indir / "coll.json", "r"))
 coll = instance_from_dict(d)
 assert coll.nrelations == 6
-
-
-# -- Test loading collection
-#collfile = outdir / "coll0.json"
-#
-## Alt. 1: If we know that we only have one collection in the storage
-#with dlite.Storage("json", collfile, "mode=r") as s:
-#    generator = s.instances(dlite.COLLECTION_ENTITY)
-#    coll2 = generator.next()
-#    if generator.next():
-#        raise dlite.DLiteStorageLoadError(
-#            "Storage '{collfile}' contain more than one collection. "
-#            "`id` is needed to select which one to load"
-#        )
-#assert coll2.nrelations == 8
-#
-## Alt. 2: Make a list of all collections in the storage
-#with dlite.Storage("json", collfile, "mode=r") as s:
-#    collections = list(s.instances(dlite.COLLECTION_ENTITY))
-#assert len(collections) == 1
-#assert collections[0].nrelations == 8
