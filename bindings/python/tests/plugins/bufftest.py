414deff81c468f28746fbdea74c47804297198ff
Jesper Friis <jesper.friis@sintef.no>
2024-10-12 19:56:19 +0200

    Try to correct python install dirs on Windows

---
+++ bindings/python/tests/plugins/bufftest.py
@@ -0,0 +1,19 @@
+"""Test plugin that only defines to_bytes() and from_bytes()."""
+
+import dlite
+
+class bufftest(dlite.DLiteStorageBase):
+    """Test plugin that represents instances as byte-encoded json."""
+
+    def open(self, location, options=None):
+        """Open storage."""
+
+    @classmethod
+    def to_bytes(cls, inst, options=None):
+        """Returns instance as bytes."""
+        return str(inst).encode()
+
+    @classmethod
+    def from_bytes(cls, buffer, id=None, options=None):
+        """Load instance from buffer."""
+        return dlite.Instance.from_json(buffer.decode())
