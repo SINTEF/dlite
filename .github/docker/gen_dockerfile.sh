ffb39dd8294f2c5c911eb0384d0e5d895e661391
Jesper Friis <jesper.friis@sintef.no>
2024-11-25 18:13:39 +0100

    Included python versions in the generated dockerfiles

--- .github/docker/gen_dockerfile.sh
+++ .github/docker/gen_dockerfile.sh
@@ -3,21 +3,23 @@
 #
 # Usage:
 #
-#     gen_dockerfile.sh SYSTEM SYSTEM_TYPE ARCH
+#     gen_dockerfile.sh SYSTEM SYSTEM_TYPE ARCH [PY_MINORS]
 #
 # Arguments:
-#   SYSTEM: system type. Ex: manylinux, musllinux
-#   SYSTEM_TYPE: Ex: 2010, 2014, _2_24, _2_28 (manylinux), _1_1, _1_2 (musllinux)
-#   ARCH: Ex: x86_64, i686
+#   SYSTEM: system. Ex: manylinux, musllinux
+#   SYSTEM_TYPE: System type. Ex: 2010, 2014, _2_24, _2_28 (manylinux), _1_1, _1_2 (musllinux)
+#   ARCH: Architecture. Ex: x86_64, i686
+#   PY_MINORS: Optional list of Python3 minor version numbers. Default: "8 9 10 11 12"
 set -eu
 
-if [[ $# -ne 3 ]]; then
+if [[ $# -lt 3 ]]; then
     echo "Usage: gen_dockerfile.sh SYSTEM SYSTEM_TYPE ARCH"
     exit 1
 fi
 SYSTEM=$1
 SYSTEM_TYPE=$2
 ARCH=$3
+PY_MINORS="${4:=8 9 10 11 12}"
 
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd -P )
@@ -40,6 +42,7 @@ fi
 sed \
     -e "s|{{ ARCH }}|${ARCH}|" \
     -e "s|{{ TYPE }}|${SYSTEM_TYPE}|" \
+    -e "s|{{ PY_MINORS }}|${PY_MINORS}|" \
     -e "s|{{ EXTRA_PRE }}|${EXTRA_PRE}|" \
     -e "s|{{ EXTRA_POST }}|${EXTRA_POST}|" \
     $template
