5c99a4573e473a3f4073b44abf6b64adf60ffa2b
Jesper Friis <jesper.friis@sintef.no>
2025-06-16 17:41:35 +0200

    Added test file

--- bindings/python/python-protocol-plugins/zip.py
+++ bindings/python/python-protocol-plugins/zip.py
@@ -1,4 +1,4 @@
-"""DLite protocol plugin for http connections."""
+"""DLite protocol plugin for zip files."""
 from urllib.parse import urlparse
 
 import hashlib
@@ -12,25 +12,28 @@ from dlite.utils import get_cachedir
 
 
 class zip(dlite.DLiteProtocolBase):
-    """DLite protocol plugin for http and https connections."""
+    """DLite protocol plugin for zip archives."""
 
     def open(self, location, options=None):
         """Opens `location`.
 
         Arguments:
-            location: A URL or path to a file or directory.
-            options: Options will be passed as keyword arguments to
-                requests.  For available options, see
-                https://requests.readthedocs.io/en/latest/api/#requests.request
-                A timeout of 1 second will be added by default.
+            location: A URL or path to a zip file.
+                If `location` is a URL, a local cache is created and reused.
+            options: The following options are supported:
+                - timeout: Number of seconds before timing out when downloading
+                  an URL.
+                - nocache: If true and path is a URL, the zip file will be
+                  downloaded again, even if a cache exists.
         """
-        opts = Options(options, "timeout=1")
+        opts = Options(options, "timeout=1;nocache=no")
+        nocache = dlite.asbool(opts["nocache"])
         path, zippath = location.split("#", 1)
 
         if re.match("^https?:.*", path):
             key = hashlib.shake_128(path.encode()).hexdigest(6)
             zipfile = get_cachedir() / f"cache-{key}.zip"
-            if not self.zipfile.exists():
+            if nocache or not self.zipfile.exists():
                 r = requests.get(path, timeout=float(opts["timeout"]))
                 r.raise_for_status()
                 with open(self.zipfile, "wb") as f:
