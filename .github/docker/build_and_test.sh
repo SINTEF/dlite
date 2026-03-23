fc23af4ec3ef533c7c0dbc0203f263f5f3bebb11
Jesper Friis <jesper.friis@sintef.no>
2026-03-23 08:40:56 +0100

    Added dockerfile for ci-testing

--- .github/docker/build_and_test.sh
+++ .github/docker/build_and_test.sh
@@ -4,36 +4,25 @@
 # Exit on errors and treat unbound variables as errors
 set -eu
 
-SYSTEM=$(uname -s)
-TYPE=""
-ARCH=$(arch)
 #PY_MINORS="10 11 12 13 14"
 PY_MINORS="10 14"
 
 
-echo "SYSTEM=$SYSTEM"
-echo "TYPE=$TYPE"
-echo "ARCH=$ARCH"
-echo "PY_MINORS=$PY_MINORS"
-
 for minor in $PY_MINORS; do
 
     python=python3.$minor
-    tmpdir=/tmp/dlite/cp3${minor}-${SYSTEM}${TYPE}_${ARCH}
-    tmpdir=/tmp/dlite/cp3${minor}-${SYSTEM}${TYPE}_${ARCH}
+    tmpdir=/tmp/cp3${minor}
+    venvdir=$tmpdir/venv
     builddir=$tmpdir/build
 
-    mkdir -p $builddir
-
-    # Create virtual environment
-    #venvdir=$tmpdir/venv
-    #$python -m venv $venvdir
-    #source $venvdir/bin/activate
+    # Activate virtual environment
+    source $venvdir/bin/activate
 
     # Build, install and test DLite
+    mkdir -p $builddir
     cd $builddir
-    #cmake -DCMAKE_INSTALL_PREFIX=$tmpdir /io
-    cmake -DPYTHON_VERSION=3.$minor /io
+    cmake -DCMAKE_INSTALL_PREFIX=$tmpdir /io
+    #cmake -DPYTHON_VERSION=3.$minor /io
     cmake --build . -j4
     cmake --install .
     ctest -j4
